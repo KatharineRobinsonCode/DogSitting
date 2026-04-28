ae568620826fe8ea085f33f373dc8ae8c900379f
KatharineRobinsonCode <katharinerobinsonltdcode@gmail.com>
2026-04-28 21:50:02 +0100

    TRYING TO FIGURE OUT NAVMESHFLOORS

--- Assets/Scripts/BarkTrigger.cs
+++ Assets/Scripts/BarkTrigger.cs
@@ -6,11 +6,12 @@ public class BarkTrigger : MonoBehaviour
     [SerializeField] private AudioClip barkClip;
 
     private void OnTriggerEnter(Collider other)
+{
+    Debug.Log($"[BarkTrigger] Hit by: {other.gameObject.name} tag: {other.tag}");
+    if (other.CompareTag("Player"))
     {
-        if (other.CompareTag("Player"))
-        {
-            barkAudio.PlayOneShot(barkClip);
-            gameObject.SetActive(false); // fire once then disable
-        }
+        barkAudio.PlayOneShot(barkClip);
+        gameObject.SetActive(false);
     }
+}
 }
